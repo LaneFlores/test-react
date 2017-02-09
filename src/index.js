cc5cf61a9dfcf707515f9e65e7387b6a0413000f
LordKahra <lordkahra@gmail.com>
2017-02-09 13:43:34 -0500

    Gitignore, JQuery Dependency

--- src/index.js
+++ src/index.js
@@ -4,8 +4,58 @@ import $ from "jquery";
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+var init = function() {
+    ReactDOM.render(
+        <App />,
+        document.getElementById('root')
+    );
 
+    console.log("Hello world?!");
+
+    const element = <ThingList />;
+    ReactDOM.render(element, document.getElementById('root'));
+};
+
+export default class ThingList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            person: [],
+            data: []
+        };
+    }
+
+    componentDidMount() {
+        this.ThingList();
+    }
+
+    ThingList() {
+        return $.getJSON(API_HOST + 'journal')
+            .then((data) => {
+                console.log("Received data:");
+                console.log(data);
+                this.setState({
+                    //person: data.data,
+                    data: data
+                });
+            });
+    }
+
+    render() {
+        console.log("ThingList.render(): Entered.");
+        console.log("ThingList.render(): this.state:");
+        console.log(this.state);
+        const persons = this.state.data.map((item, i) => {
+            return <div>
+                <h1>{item.data}</h1>
+            </div>
+        });
+
+        return <div id="layout-content" className="layout-content-wrapper">
+            <div className="panel-list">{ persons }</div>
+        </div>
+    }
+}
+
+init();
