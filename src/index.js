1092c94254f80b335e3182f12af6ab91e928aa7c
LordKahra <lordkahra@gmail.com>
2017-07-25 16:11:06 -0400

    Game
    Followed a short game tutorial.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,28 @@ import {
     Route,
     Link
 } from 'react-router-dom';
+import { createStore } from 'redux'
+
+function counter(state = 0, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+let store = createStore(counter);
+
+store.subscribe(() =>
+    console.log('store: ' + store.getState())
+);
+
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'DECREMENT'});
 
 /*function peopleInit() {
     console.log("peopleInit(): Entered.");
