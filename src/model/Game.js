da31252d232a4a879d00a12b143f7891031ab56a
Lane Flores <mflores@star2star.com>
2017-07-27 15:21:43 -0400

    Imports
    Changing up imports.

--- src/model/Game.js
+++ src/model/Game.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from "jquery";
-import App from '../App';
+//import App from '../App';
 
 function Square(props) {
     return (
