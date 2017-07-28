def178ac97a23150505c1d011bd1cae1fe08bdb6
Lane Flores <mflores@star2star.com>
2017-07-28 13:59:19 -0400

    Report - ForEach
    Trying to figure out how to forEach in a render.

--- src/model/Report.js
+++ src/model/Report.js
@@ -26,8 +26,8 @@ export default class Report extends React.Component {
         for (let key in this.props.arr_fields) {
             if (!this.props.arr_fields.hasOwnProperty(key)) continue;
             console.log("key: " + key);
-            console.log("value: " + this.props.fields[key]);
-            rows.push(<th>{this.props.fields[key]}</th>);
+            console.log("value: " + this.props.arr_fields[key]);
+            rows.push(<th>{this.props.arr_fields[key]}</th>);
         }
         //return (<section>{this.props.fields}</section>);
         return (
