2220e9901ee0844f7abcb6723887e49c114c6c50
Lane Flores <mflores@star2star.com>
2017-08-01 13:28:02 -0400

    Report - ForEach
    Trying to figure out how to forEach in a render.

--- src/index.js
+++ src/index.js
@@ -131,7 +131,7 @@ const BasicExample = () => (
             <Route exact path="/about" component={About}/>
             <Route exact path="/topics" component={Topics}/>
             /*<Route exact path="/report" component={ReportPage}/>*/
-            <Route exact path="/report" component={reportView.render()}/>
+            <Route exact path="/report" component={reportView}/>
         </div>
     </Router>
 );
